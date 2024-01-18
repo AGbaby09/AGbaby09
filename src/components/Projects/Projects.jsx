545ac7076ac7bf64cfd2a30f7a773b5937e4bf2a
AGbaby09 <agbotsem@gmail.com>
2024-01-18 01:15:36 +0000

    second commit, please work

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,8 @@
 import './Projects.css'
+import ten from '../../assets/ten.png'
+import avatar from '../../assets/avatar.png'
+import tabbed from '../../assets/tabbed.png'
+import art from '../../assets/art.png'
 
 const Projects = () => {
 
@@ -7,6 +11,48 @@ const Projects = () => {
             <div className="MyWorks">
                 <p>Brouwse through my</p>
                 <h1>Works</h1>
+            </div>
+            <div id="Works" className='scrollable'>
+                <div className="work">
+                    <p className="workTitle">Mila solves problems</p>
+                    <img src={ten} alt="project preview" className="full" />
+                    <button className="livePreview center">
+                        <a href="" className='center'> Live Preview <i className='bx bx-link'></i>  </a> 
+                    </button>
+                    <button className="sourceCode center">
+                        <a href="" className='center'><i className='bx bxl-github'></i> Source code </a> 
+                    </button>
+                </div>
+                <div className="work">
+                    <p className="workTitle">Simplest way to create forms</p>
+                    <img src={avatar} alt="project preview" className="full" />
+                    <button className="livePreview center">
+                        <a href="" className='center'> Live Preview <i className='bx bx-link'></i>  </a> 
+                    </button>
+                    <button className="sourceCode center">
+                        <a href="" className='center'><i className='bx bxl-github'></i> Source code </a> 
+                    </button>
+                </div>
+                <div className="work">
+                    <p className="workTitle">Fun, engaging, and authentic</p>
+                    <img src={tabbed} alt="project preview" className="full" />
+                    <button className="livePreview center">
+                        <a href="" className='center'> Live Preview <i className='bx bx-link'></i>  </a> 
+                    </button>
+                    <button className="sourceCode center">
+                        <a href="" className='center'><i className='bx bxl-github'></i> Source code </a> 
+                    </button>
+                </div>
+                <div className="work">
+                    <p className="workTitle">Art Director</p>
+                    <img src={art} alt="project preview" className="full" />
+                    <button className="livePreview center">
+                        <a href="" className='center'> Live Preview <i className='bx bx-link'></i>  </a> 
+                    </button>
+                    <button className="sourceCode center">
+                        <a href="" className='center'><i className='bx bxl-github'></i> Source code </a> 
+                    </button>
+                </div>
             </div>
         </section>
     )
