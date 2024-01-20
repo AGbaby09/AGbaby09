e658718fcf9f4a79aebd3c2045678d795ec6d59e
AGbaby09 <agbotsem@gmail.com>
2024-01-20 00:29:54 +0000

    NINth commit, please work

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,24 @@
 import './Contact.css'
+import adminBG from '../../assets/minimal_dashboard.png'
+import arrow from '../../assets/arrow.png'
 
 const Contact = () => {
 
     return (
         <section id="Contact">
-            
+            <div id="contactBG">
+                <img src={adminBG} alt="" className="full" />
+            </div>
+            <div id="content">
+                <h1>Turning Vision into Digital Reality: Your Ideas, Elevated. Let's Connect for Tailored Full-Stack Development Solutions.</h1>
+                <button className='center'>Let's talk</button>
+                
+                <form>
+                    <input type="text" placeholder='Enter your email'/>
+                    <textarea name="" id="" cols="50" rows="10" placeholder='Enter your message' />
+                    <button>Send a message</button>
+                </form>
+            </div>
         </section>
     )
 }
