5e713daf81e3c0c47ec248ef8f6e94a62679065b
AGbaby09 <agbotsem@gmail.com>
2024-01-20 00:44:21 +0000

    NINth commit, please work

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,11 +13,6 @@ const Contact = () => {
                 <h1>Turning Vision into Digital Reality: Your Ideas, Elevated. Let's Connect for Tailored Full-Stack Development Solutions.</h1>
                 <button className='center'>Let's talk</button>
                 
-                <form>
-                    <input type="text" placeholder='Enter your email'/>
-                    <textarea name="" id="" cols="50" rows="10" placeholder='Enter your message' />
-                    <button>Send a message</button>
-                </form>
             </div>
         </section>
     )
